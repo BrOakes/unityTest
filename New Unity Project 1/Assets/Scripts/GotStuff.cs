528e8ea457246907edcfa848b4db21a488d3846f
BrOakes <BrOakesGames@gmail.com>
2016-06-16 22:45:21 -0400

    in ap

---
+++ New Unity Project 1/Assets/Scripts/GotStuff.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+
+public class GotStuff : MonoBehaviour
+{
+
+    public Text stuffText = null;
+
+	// Use this for initialization
+	void Start ()
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	
+	}
+
+    public void setStuff()
+    {
+        stuffText.text = "You got stuff";
+    }
+}
